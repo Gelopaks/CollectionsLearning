e9fb1701873076c8495ce42c54a1d7662412d078
future <pak.ol@yahoo.com>
2023-04-01 13:51:11 +0700

    Cats ArrayList example

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -1,5 +1,6 @@
 package ru.synergy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
@@ -9,11 +10,18 @@ public class Main {
         cats[0] = new Cat("Томас");
         cats[1] = new Cat("Бегемот");
         cats[2] = new Cat("Филипп Маркович");
-
         //cats[3] = new Cat("Котяра"); // нельзя добавить запись т.к. размер массива 3
-
         //cats[0] = null; // можно присвоить нулевое значение
-
         System.out.println(Arrays.toString(cats));
+
+// создаем динамический массив
+        ArrayList<Cat> catsList = new ArrayList<>();
+
+        for (Cat cat : cats){
+            catsList.add(cat);
+        }
+        catsList.add(new Cat("Котяра"));
+
+        System.out.println(catsList.toString());
     }
 }
