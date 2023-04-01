36bccd4e6dd81c7e8cf3c7cc6de0706dfa558088
future <pak.ol@yahoo.com>
2023-04-01 14:52:05 +0700

    ArrayList example with cats completed

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -16,12 +16,24 @@ public class Main {
 
 // создаем динамический массив
         ArrayList<Cat> catsList = new ArrayList<>();
-
         for (Cat cat : cats){
             catsList.add(cat);
         }
-        catsList.add(new Cat("Котяра"));
+
+        catsList.add(new Cat("Бегемот2"));// добовляет запись в массив
+        catsList.remove(1); // удаляем запись из массива
+
+        Cat cat = catsList.get(0);
+        System.out.println(cat);
+        System.out.println(catsList.indexOf(cat));
+
+        catsList.add(2, cat);
+        catsList.set(2, new Cat("Меня сюда вставили"));
+
+        catsList.removeAll(Arrays.asList(cat, catsList.get(3))); // удаляет выбранный записи
+        // как по имени переменной так и по индексу
 
         System.out.println(catsList.toString());
+        System.out.println(catsList.size());
     }
 }
