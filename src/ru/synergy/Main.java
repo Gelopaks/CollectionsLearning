6faa4f1ba84a772be8ff762dfb2197098c17c994
future <pak.ol@yahoo.com>
2023-04-01 13:08:37 +0700

    Cats example

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -1,7 +1,19 @@
 package ru.synergy;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
+//Arrays
+        Cat[] cats = new Cat[3];
+        cats[0] = new Cat("Томас");
+        cats[1] = new Cat("Бегемот");
+        cats[2] = new Cat("Филипп Маркович");
 
+        //cats[3] = new Cat("Котяра"); // нельзя добавить запись т.к. размер массива 3
+
+        //cats[0] = null; // можно присвоить нулевое значение
+
+        System.out.println(Arrays.toString(cats));
     }
 }
